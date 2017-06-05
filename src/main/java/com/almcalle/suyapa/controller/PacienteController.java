857499d3b8cab0216a810023239e40e2acb03a36
almcalle <alfonso@MacBook-Air-de-Alfonso.local>
2017-06-05 18:48:31 +0200

    Añadido detalle
    Añadido detalle de los pacientes

--- src/main/java/com/almcalle/suyapa/controller/PacienteController.java
+++ src/main/java/com/almcalle/suyapa/controller/PacienteController.java
@@ -1,6 +1,7 @@
 package com.almcalle.suyapa.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.almcalle.suyapa.domain.Paciente;
@@ -27,15 +29,10 @@ public class PacienteController {
 	
 	 @GetMapping("/pacientes")
 	    public String listaPaciente(Model model) {
-		 log.debug("Accediendo a /pacientes");
-		 
-		 
-		 
+		 log.debug("Accediendo a /pacientes");	 
 		List<Paciente> pacientes = pacienteRepository.findAll();
-		 log.debug("Lista de pacientes: " + pacientes.toString());
-		 
-	        model.addAttribute("pacientes", pacientes );
-	       
+		 log.debug("Lista de pacientes: " + pacientes.toString());		 
+	        model.addAttribute("pacientes", pacientes );	       
 	        return "paciente/listPacientes";
 	    }
 	
@@ -43,19 +40,34 @@ public class PacienteController {
 	
     @GetMapping("/paciente/add")
     public String mostrarFormPaciente(Model model) {
+		 log.debug("Accediendo a /paciente/add");
     	 model.addAttribute("paciente", new Paciente());
         return "paciente/addPaciente";
     }
 
     @PostMapping("/paciente")
-    public String guardarPaciente(@ModelAttribute Paciente paciente) {
-    	
-    	
-    	pacienteRepository.save(paciente);
-    	
-    	
-    	
+    public String guardarPaciente(@ModelAttribute Paciente paciente) {    	
+		 log.debug("Guardando Paciente");    	
+    	pacienteRepository.save(paciente);       	
         return "paciente/detallePaciente";
     }
 
+    
+	@GetMapping("/paciente/{id}")
+	public String detallePaciente(Model model, @PathVariable(value = "id") Long id) {
+		log.debug("Detalle Paciente del paciente: " + id);
+		Paciente paciente = pacienteRepository.findById(id).orElse(null);
+
+		if (paciente != null) {
+			log.debug("Paciente obtenido: " + paciente);
+
+			model.addAttribute("paciente", paciente);
+
+			return "paciente/detallePaciente";
+		} else
+			return "paciente/noPaciente";
+
+	}
+    
+    
 }
